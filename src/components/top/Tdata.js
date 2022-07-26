49e1edbd4e86cc97ddf9188f5d04e21e36dbb0a1
dvhuzg <dohungdvh@gmail.com>
2022-07-26 11:21:38 +0700

    complete Category

---
+++ src/components/top/Tdata.js
@@ -0,0 +1,32 @@
+import c_1 from '../../common/assets/images/Top/category-1.png';
+import c_2 from '../../common/assets/images/Top/category-2.png';
+import c_3 from '../../common/assets/images/Top/category-3.png';
+const Tdata = [
+    {
+      cover: c_1,
+      para: "headphone",
+      desc: "3k orders this week",
+    },
+    {
+      cover: c_2,
+      para: "watch",
+      desc: "4k orders this week",
+    },
+    {
+      cover: c_3,
+      para: "sunglass",
+      desc: "6k orders this week",
+    },
+    {
+      cover: c_2,
+      para: "watch",
+      desc: "4k orders this week",
+    },
+    {
+      cover: c_3,
+      para: "sunglass",
+      desc: "6k orders this week",
+    },
+  ]
+  
+  export default Tdata
